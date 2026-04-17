a403ea85db8ad1df6ec5ace5264166ef9d7f6004
Divolin Lensy K <diboninlensy@gmail.com>
2026-04-17 12:32:51 +0530

    Add files via upload

---
+++ IoT Monitoring.py
@@ -0,0 +1,31 @@
+import random
+import requests
+import time
+
+
+api_key = "4D50H2M7EFMZFOSX"
+
+while True:
+    # 📊 Random health data generate
+    heart_rate = random.randint(60, 120)
+    temperature = round(random.uniform(36, 40), 2)
+
+    # 🖥️ Print values
+    print("Heart Rate:", heart_rate)
+    print("Temperature:", temperature)
+
+    # ⚠️ Alert conditions
+    if heart_rate > 100:
+        print("⚠️ High Heart Rate Alert!")
+
+    if temperature > 38:
+        print("⚠️ Fever Alert!")
+
+    # 🌐 Send data to ThingSpeak
+    url = f"https://api.thingspeak.com/update?api_key={api_key}&field1={heart_rate}&field2={temperature}"
+    response = requests.get(url)
+
+    print("Data Sent to ThingSpeak\n")
+
+    # ⏱️ 15 seconds delay (ThingSpeak limit)
+    time.sleep(10)
